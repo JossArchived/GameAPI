495ef29f7700de6f2e053f01db1ea9bcd9129e2c
Josscoder <josscoder@hotmail.com>
2021-10-05 16:56:56 -0500

    feat: now broadcasts serve with spectators and you can already get the spectators and their quantity.

--- src/main/java/jossc/game/state/GameState.java
+++ src/main/java/jossc/game/state/GameState.java
@@ -8,9 +8,8 @@ import cn.nukkit.network.protocol.PlaySoundPacket;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.scheduler.TaskHandler;
 import cn.nukkit.utils.TextFormat;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 import net.minikloon.fsmgasm.State;
 
 public abstract class GameState extends State implements Listener {
@@ -45,8 +44,36 @@ public abstract class GameState extends State implements Listener {
     tasks.clear();
   }
 
+  protected final Collection<? extends Player> getSpectators() {
+    return new ArrayList<Player>(getPlayers(Player.SPECTATOR));
+  }
+
   protected final Collection<? extends Player> getPlayers() {
-    return Server.getInstance().getOnlinePlayers().values();
+    List<Player> players = new ArrayList<>();
+
+    players.addAll(getPlayers(Player.SURVIVAL));
+    players.addAll(getPlayers(Player.ADVENTURE));
+    players.addAll(getPlayers(Player.CREATIVE));
+
+    return players;
+  }
+
+  protected final Collection<? extends Player> getPlayers(int gamemode) {
+    return Server
+      .getInstance()
+      .getOnlinePlayers()
+      .values()
+      .stream()
+      .filter(player -> player.getGamemode() == gamemode)
+      .collect(Collectors.toList());
+  }
+
+  protected int playersSize() {
+    return getPlayers().size();
+  }
+
+  protected int spectatorsSize() {
+    return getSpectators().size();
   }
 
   protected final void playSound(Player player, String soundName) {
@@ -62,33 +89,96 @@ public abstract class GameState extends State implements Listener {
   }
 
   protected final void broadcastSound(String soundName) {
-    getPlayers().forEach(player -> playSound(player, soundName));
+    broadcastSound(soundName, false);
+  }
+
+  protected final void broadcastSound(String soundName, boolean toSpectator) {
+    if (toSpectator) {
+      getSpectators().forEach(spectator -> playSound(spectator, soundName));
+    } else {
+      getPlayers().forEach(player -> playSound(player, soundName));
+    }
   }
 
   protected final void broadcastMessage(String message) {
-    getPlayers()
-      .forEach(player -> player.sendMessage(TextFormat.colorize(message)));
+    broadcastMessage(message, false);
+  }
+
+  protected final void broadcastMessage(String message, boolean toSpectator) {
+    if (toSpectator) {
+      getSpectators()
+        .forEach(
+          spectator -> spectator.sendMessage(TextFormat.colorize(message))
+        );
+    } else {
+      getPlayers()
+        .forEach(player -> player.sendMessage(TextFormat.colorize(message)));
+    }
   }
 
   protected final void broadcastActionBar(String title) {
-    getPlayers()
-      .forEach(player -> player.sendActionBar(TextFormat.colorize(title)));
+    broadcastActionBar(title, false);
+  }
+
+  protected final void broadcastActionBar(String title, boolean toSpectator) {
+    if (toSpectator) {
+      getSpectators()
+        .forEach(
+          spectator -> spectator.sendActionBar(TextFormat.colorize(title))
+        );
+    } else {
+      getPlayers()
+        .forEach(player -> player.sendActionBar(TextFormat.colorize(title)));
+    }
+  }
+
+  protected final void broadcastActionBar(
+    String title,
+    int fadeIn,
+    int stay,
+    int fadeOut
+  ) {
+    broadcastActionBar(title, fadeIn, stay, fadeOut, false);
+  }
+
+  protected final void broadcastActionBar(
+    String title,
+    int fadeIn,
+    int stay,
+    int fadeOut,
+    boolean toSpectator
+  ) {
+    if (toSpectator) {
+      getSpectators()
+        .forEach(
+          spectator ->
+            spectator.sendActionBar(
+              TextFormat.colorize(title),
+              fadeIn,
+              stay,
+              fadeOut
+            )
+        );
+    } else {
+      getPlayers()
+        .forEach(
+          player ->
+            player.sendActionBar(
+              TextFormat.colorize(title),
+              fadeIn,
+              stay,
+              fadeOut
+            )
+        );
+    }
   }
 
   protected final void broadcastTitle(String title) {
-    getPlayers()
-      .forEach(player -> player.sendTitle(TextFormat.colorize(title)));
+    broadcastTitle(title, false);
   }
 
   protected final void broadcastTitle(String title, String subTitle) {
-    getPlayers()
-      .forEach(
-        player ->
-          player.sendTitle(
-            TextFormat.colorize(title),
-            TextFormat.colorize(subTitle)
-          )
-      );
+    broadcastTitle(title, subTitle, false);
   }
 
   protected final void broadcastTitle(
@@ -98,49 +188,108 @@ public abstract class GameState extends State implements Listener {
     int stay,
     int fadeOut
   ) {
-    getPlayers()
-      .forEach(
-        player ->
-          player.sendTitle(
-            TextFormat.colorize(title),
-            TextFormat.colorize(subTitle),
-            fadeIn,
-            stay,
-            fadeOut
-          )
-      );
+    broadcastTitle(title, subTitle, fadeIn, stay, fadeOut, false);
+  }
+
+  protected final void broadcastTitle(String title, boolean toSpectator) {
+    if (toSpectator) {
+      getSpectators()
+        .forEach(spectator -> spectator.sendTitle(TextFormat.colorize(title)));
+    } else {
+      getPlayers()
+        .forEach(player -> player.sendTitle(TextFormat.colorize(title)));
+    }
+  }
+
+  protected final void broadcastTitle(
+    String title,
+    String subTitle,
+    boolean toSpectator
+  ) {
+    if (toSpectator) {
+      getSpectators()
+        .forEach(
+          spectator ->
+            spectator.sendTitle(
+              TextFormat.colorize(title),
+              TextFormat.colorize(subTitle)
+            )
+        );
+    } else {
+      getPlayers()
+        .forEach(
+          player ->
+            player.sendTitle(
+              TextFormat.colorize(title),
+              TextFormat.colorize(subTitle)
+            )
+        );
+    }
+  }
+
+  protected final void broadcastTitle(
+    String title,
+    String subTitle,
+    int fadeIn,
+    int stay,
+    int fadeOut,
+    boolean toSpectator
+  ) {
+    if (toSpectator) {
+      getSpectators()
+        .forEach(
+          spectator ->
+            spectator.sendTitle(
+              TextFormat.colorize(title),
+              TextFormat.colorize(subTitle),
+              fadeIn,
+              stay,
+              fadeOut
+            )
+        );
+    } else {
+      getPlayers()
+        .forEach(
+          player ->
+            player.sendTitle(
+              TextFormat.colorize(title),
+              TextFormat.colorize(subTitle),
+              fadeIn,
+              stay,
+              fadeOut
+            )
+        );
+    }
   }
 
   protected final void broadcastPopup(String message) {
-    getPlayers()
-      .forEach(player -> player.sendPopup(TextFormat.colorize(message)));
+    broadcastPopup(message, false);
+  }
+
+  protected final void broadcastPopup(String message, boolean toSpectator) {
+    if (toSpectator) {
+      getSpectators()
+        .forEach(
+          spectator -> spectator.sendPopup(TextFormat.colorize(message))
+        );
+    } else {
+      getPlayers()
+        .forEach(player -> player.sendPopup(TextFormat.colorize(message)));
+    }
   }
 
   protected final void broadcastTip(String message) {
-    getPlayers()
-      .forEach(player -> player.sendTip(TextFormat.colorize(message)));
+    broadcastTip(message, false);
   }
 
-  protected int playersSize() {
-    return getPlayers().size();
-  }
-
-  protected final void broadcastActionBar(
-    String title,
-    int fadeIn,
-    int stay,
-    int fadeOut
-  ) {
-    getPlayers()
-      .forEach(
-        player ->
-          player.sendActionBar(
-            TextFormat.colorize(title),
-            fadeIn,
-            stay,
-            fadeOut
-          )
-      );
+  protected final void broadcastTip(String message, boolean toSpectator) {
+    if (toSpectator) {
+      getSpectators()
+        .forEach(spectator -> spectator.sendTip(TextFormat.colorize(message)));
+    } else {
+      getPlayers()
+        .forEach(player -> player.sendTip(TextFormat.colorize(message)));
+    }
   }
 
   protected void register(Listener listener) {
